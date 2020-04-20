ea4bc0cce9e02be5940b83234a60d6e177000b26
John Nordberg <john.nordberg@gmail.com>
2020-04-21 00:20:25 +0300

    Update copyright boilerplate

--- Diwen.BofCrypt/EncryptedReport.cs
+++ Diwen.BofCrypt/EncryptedReport.cs
@@ -1,11 +1,23 @@
-
-// <?xml version="1.0" encoding="utf-8"?>
-// <ENCRYPTED_REPORT xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-//   <TITLE>BOFCRYPTNXT</TITLE>
-//   <VERSION>2.0</VERSION>
-//   <SESSION_KEY>Base64Enc(RSAEncrypt($FivaPublicKey, $SessionKey))</SESSION_KEY>
-//   <OUT_BUFFER>Base64Enc((InitializationVector)(AESEncrypt($SessionKey, $PlainTextData)))</OUT_BUFFER>
-// </ENCRYPTED_REPORT>
+//
+//  This file is part of Diwen.BofCrypt.
+//
+//  Author:
+//       John Nordberg <john.nordberg@gmail.com>
+//
+//  Copyright (c) 2020 John Nordberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace Diwen.BofCrypt
 {
