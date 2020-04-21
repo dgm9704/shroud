881a524ffdc2b38cfef24e386e465d75b0b1436b
John Nordberg <john.nordberg@gmail.com>
2020-04-21 22:45:53 +0300

    #1 better serialization, add versioninfo to report

--- Diwen.BofCrypt/EncryptedReport.cs
+++ Diwen.BofCrypt/EncryptedReport.cs
@@ -21,12 +21,39 @@
 
 namespace Diwen.BofCrypt
 {
+    using System;
     using System.IO;
+    using System.Reflection;
+    using System.Text;
+    using System.Xml;
     using System.Xml.Serialization;
 
     [XmlRoot("ENCRYPTED_REPORT")]
     public class EncryptedReport
     {
+        static AssemblyName assembly = Assembly.GetExecutingAssembly().GetName();
+        static Version version = assembly.Version;
+        static string id = assembly.Name;
+
+        static XmlSerializer Serializer = new XmlSerializer(typeof(EncryptedReport));
+
+        static XmlReaderSettings XmlReaderSettings = new XmlReaderSettings
+        {
+            IgnoreWhitespace = true,
+            IgnoreProcessingInstructions = false,
+            IgnoreComments = false,
+            XmlResolver = null,
+            ValidationType = ValidationType.None
+        };
+
+        static XmlWriterSettings XmlWriterSettings = new XmlWriterSettings
+        {
+            Indent = true,
+            IndentChars = "\t",
+            NamespaceHandling = NamespaceHandling.OmitDuplicates,
+            Encoding = Encoding.UTF8
+        };
+
         [XmlElement("TITLE")]
         public string Title { get; set; } = "BOFCRYPTNXT";
 
@@ -39,27 +66,74 @@ namespace Diwen.BofCrypt
         [XmlElement("OUT_BUFFER")]
         public string OutBuffer { get; set; } = string.Empty;
 
-        public void ToFile(string path)
+        public void ToStream(Stream stream)
         {
-            using (var stream = new FileStream(path, FileMode.Create, FileAccess.Write))
-                this.ToStream(stream);
+            using (var writer = XmlWriter.Create(stream, XmlWriterSettings))
+                ToXmlWriter(writer);
         }
 
-        public string ToXml()
+        public static EncryptedReport FromFile(string path)
         {
-            using (var writer = new StringWriter())
+            EncryptedReport report;
+
+            using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                report = FromStream(stream);
+
+            return report;
+        }
+
+        public void ToFile(string path)
+        {
+            using (var writer = XmlWriter.Create(path, XmlWriterSettings))
+                ToXmlWriter(writer);
+        }
+
+        void ToXmlWriter(XmlWriter writer)
+        {
+            var ns = new XmlSerializerNamespaces();
+            var info = $" {id} {version} ";
+            Serializer.Serialize(writer, this, ns);
+            writer.WriteComment(info);
+        }
+
+        public XmlDocument ToXmlDocument()
+        {
+            var document = new XmlDocument();
+            var declaration = document.CreateXmlDeclaration("1.0", "UTF-8", null);
+            document.AppendChild(declaration);
+            var nav = document.CreateNavigator();
+
+            using (var writer = nav.AppendChild())
+                ToXmlWriter(writer);
+
+            return document;
+        }
+
+        public static EncryptedReport FromXml(string content)
+        {
+            using (var stream = new MemoryStream())
+            using (var writer = new StreamWriter(stream))
             {
-                var xml = new XmlSerializer(typeof(EncryptedReport));
-                xml.Serialize(writer, this);
-                return writer.ToString();
+                writer.Write(content);
+                writer.Flush();
+
+                stream.Position = 0;
+
+                return FromStream(stream);
             }
         }
 
-        public void ToStream(Stream stream)
+        public static EncryptedReport FromStream(Stream stream)
         {
-            var xml = new XmlSerializer(typeof(EncryptedReport));
-            xml.Serialize(stream, this);
+            EncryptedReport report;
+            using (var reader = XmlReader.Create(stream, XmlReaderSettings))
+                report = (EncryptedReport)Serializer.Deserialize(reader);
+
+            return report;
         }
+
+        public string ToXml()
+        => ToXmlDocument().OuterXml;
 
     }
 
